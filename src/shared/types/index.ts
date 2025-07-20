9808030ff42d75540cf99d2846d936b447e6eba5
Максим <makwest5008@gmail.com>
2025-07-20 21:34:07 +0300

    alpha 1

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -8,7 +8,6 @@ export type { Coin, GetAxiosCoin } from './Coin.types';
 export type { WalletPreviewType } from './Wallet.types';
 export type {
   UserType,
-  UserLoginType,
   UserRegistration,
   UserLoginRequestGet,
   UserRegistrationRequestGet,
